b3162e9bf475e6b585a298378fe63239ea90bb3b
nx. <norvyxcefron@gmail.com>
2026-07-01 18:04:19 +0545

    fss

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -1,7 +1,6 @@
-
 set fish_greeting ""
-
 #glances, btop, cava, htop, fastfetch, cmatrix, pipes.sh, :)
+#
 #Gammastep
 alias g27k='gammastep -O 2700 & disown'
 alias g3k='gammastep -O 3000 & disown'
@@ -13,7 +12,6 @@ alias pgs='pkill gammastep'
 alias ck='tty-clock -c -s -C 7 -t'
 
 #Shortcuts
-#
 alias c='clear'
 alias nau='nautilus & disown'
 alias w='waybar & disown'
@@ -22,16 +20,7 @@ alias k='pkill -9 -u nikesh'
 alias re='OCL_ICD_VENDORS=/etc/OpenCL/vendors/intel.icd davinci-resolve & disown'
 alias bo='flatpak run com.usebottles.bottles & disown'
 
-function enc
-    for f in $argv[1]/*.mp4
-        set out $argv[2]/(basename $f .mp4).mov
-        if test -f $out
-            echo "Skipping $f, already exists"
-        else
-            ffmpeg -i $f -c:v dnxhd -profile:v dnxhr_hq -c:a pcm_s16le $out
-        end
-    end
-end
+
 
 function enc
     for f in $argv[1]/*.mp4
@@ -39,8 +28,22 @@ function enc
         if test -f $out
             echo "Skipping $f, already exists"
         else
-            # Changed profile to dnxhr_lb (Low Bandwidth) for massive space savings
             ffmpeg -i $f -c:v dnxhd -profile:v dnxhr_lb -c:a pcm_s16le $out
         end
     end
 end
+
+
+function dec
+    ffmpeg -i $argv[1] -c:v libx264 -crf 22 -c:a aac -movflags +faststart (test (count $argv) -ge 2; and echo $argv[2]; or echo (string replace -r '\.[^.]+$' '' $argv[1])".mp4")
+end
+
+#Run Gems directly
+set -gx PATH $HOME/.local/bin $PATH
+
+#Convert mp3
+function m3
+    ffmpeg -i $argv[1] -c:a pcm_s24le (string replace -r '\.[^.]+$' '.wav' $argv[1])
+end
+    
+alias gnu='GTK_THEME=Adwaita gnusim8085 & disown'
